348751870a9be8d429eed824f0a05b8a46c0f6e6
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-03-02 20:53:04 +0500

    add dz 15

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int PosSum(int[] array)
+{
+    int res = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        res += array[i];
+    }
+    return res;
+}
+
+int[] myArray = GenerateArray(6, -10, 10);
+PrintArray(myArray);
+System.Console.WriteLine(PosSum(myArray));
